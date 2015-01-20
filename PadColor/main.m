2914ddf702264ea4c7e670e3559887079e0ff3d6
fortinososa <smonterrubio@gmail.com>
2015-01-19 19:31:55 -0800

    Initial Commit

---
+++ PadColor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PadColor
+//
+//  Created by user27575 on 1/19/15.
+//  Copyright (c) 2015 Fortino Sosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
